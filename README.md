0ea87849d8d073a911499bb5edfc7d17b96cbe21
Marcos Barrera <mabarreraweb@gmail.com>
2023-11-19 14:48:33 -0500

    start project

---
+++ README.md
@@ -0,0 +1,1 @@
+#### AWS TS LAMBDA
---
+++ src/welcome.ts
@@ -0,0 +1,8 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const hello:APIGatewayProxyHandler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ hello: 'Hi welcome' })
+  }
+}
